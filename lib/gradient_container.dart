393086211a2b697a1cf5ecb0e16740d384b212bb
Shah-R60 <shahrahul3600@gmail>
2024-08-30 21:47:18 +0530

    app complete

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
+import 'package:first_app/dice_roller.dart';
+// import 'dar
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors1, this.colors2, {super.key});
   final Color colors1;
   final Color colors2;
+  rollDice() {}
   @override
   Widget build(context) {
     return Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [colors1, colors2],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-        ),
-        child: StyledText('Good Morning'));
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [colors1, colors2],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: Center(child: DiceRoller()),
+    );
   }
 }
 
